27b43e2f3a8a80e2be1391d0633d2359461cfa43
Amit472-ai <amitgurjar5212@gmail.com>
2025-07-17 12:45:31 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,31 @@
+// import React from "react";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import Login from "./components/Login";
+// import Signup from "./components/Signup";
+
+// function App() {
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route path="/login" element={<Login />} />
+//         <Route path="/signup" element={<Signup />} />
+//       </Routes>
+//     </Router>
+//   );
+// }
+
+// export default App;
+
+import React from "react";
+import FAQPage from "./components/FAQpage";
+
+function App() {
+  return (
+    <div className="App">
+      <FAQPage />
+    </div>
+  );
+}
+
+export default App;
+
